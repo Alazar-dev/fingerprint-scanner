1302c6e58ce2d6730565a12b52da15b7e66b30ca
Alazar-dev <mealazartadesse@gmail.com>
2024-03-14 18:36:13 +0300

    fix(bug-fix): minor bug fixes

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ import { stream } from "./lib/stream.js";
 import { findPort } from "./lib/findPort.js";
 
 
-async function main() {
+export default async function main() {
     const fingerprintDevicePort = await findPort();
 
     stream({fingerprintDevicePort});
     const data = await capture({fingerprintDevicePort});
     console.log("data is", data);
 }
-
-main();
